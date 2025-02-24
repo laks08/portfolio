415dc1279b7f53e81dd173417eb4b02ae09d55e8
Lakshya Gupta <lakshyagupta8a@gmail.com>
2025-02-23 20:41:36 -0500

    Chnaging contact to footer
    + navbar edits
    + mics

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,28 +1,16 @@
 import React from "react";
-import { FaEnvelope, FaPhone, FaLinkedin } from "react-icons/fa";
 
 const Contact = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <section id="contact" className="py-12 bg-white">
-      <div className="pt-6 max-w-7xl mx-auto px-4 text-center">
-        <h2 className="text-3xl font-bold mb-8 text-gray-600">Contact</h2>
-        <div className="space-y-4">
-          <div className="flex items-center justify-center space-x-2">
-            <FaEnvelope size={20} className="text-gray-600" />
-            <span className="text-gray-600">lakshyagupta997@gmail.com</span>
-          </div>
-          <div className="flex items-center justify-center space-x-2">
-            <FaLinkedin size={20} className="text-blue-600" />
-            <a
-              href="https://www.linkedin.com/in/lakshyagupta-/"
-              className="text-blue-600 hover:text-blue-800"
-            >
-              linkedin.com/in/lakshyagupta-
-            </a>
-          </div>
-        </div>
+    <footer className="py-6 bg-gray-100">
+      <div className="max-w-7xl mx-auto px-4 text-center">
+        <p className="text-sm text-gray-600">
+          © {currentYear} Lakshya Gupta™. All rights reserved.
+        </p>
       </div>
-    </section>
+    </footer>
   );
 };
 
